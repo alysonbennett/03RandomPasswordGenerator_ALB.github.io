26adfac7241ea4d6d67876c98d3d91771971667f
Alyson Bennett <alysonbennett@gmail.com>
2020-09-27 12:36:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Password Generator
+## Alyson Bennett
+
+The goal of this homework assignment was to create a random password generator using Javascript, with accompanying HTML and CSS files.
+
+The password that is created must meet the following criteria:
+    1. Must contain between 8 - 128 characters, AND
+    2. Must contain at least on of the following character types:
+        -lowercase letters
+        -uppercase letters
+        -numbers
+        -special characters
+
+The user must answer questions posed in prompts to generate a random string of characters that can be used as a secure password. 
---
+++ script.js
@@ -0,0 +1,26 @@
+// Define variables
+
+var letterLower = 'abcdefghijklmnopqrstuvwxyz';
+var letterUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var number = '1234567890';
+var symbol = '!@#$^&%*()+=-[]{}|:<>?,.';
+
+
+// Declare function to generate random password
+function generatePassword () {
+    var passwordLength = 
+
+
+// Ask user for password length, between 8-128 characters
+
+
+
+
+}
+// Ask user if they would like to include lower case letter, upper case letters, special characters, or numbers
+
+
+// If user replies no to all above questions, alert them to choose at least one
+
+
+// Generate random password
